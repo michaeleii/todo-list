6bbaecdfedf2618504ac70d4de86143150bb858a
Michael Lei <michaelleie0ca@gmail.com>
2023-02-04 17:13:10 -0800

    Add modal window on add task btn click

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { ToDo } from "./todo";
 import { ToDoList } from "./todo-list";
-const addButton = document.getElementById("addButton") as HTMLButtonElement;
+const addButton = document.getElementById("addTaskBtn") as HTMLButtonElement;
 
 const addToList = () => {
 	const title = document.getElementById("title") as HTMLInputElement;
@@ -19,3 +19,29 @@ const addToList = () => {
 	console.log(toDoList.getToDoList());
 };
 addButton.addEventListener("click", addToList);
+
+const openModalBtns = document.querySelectorAll(
+	".show-modal"
+) as NodeListOf<HTMLButtonElement>;
+
+const closeModalBtns = document.querySelectorAll(
+	".close-modal"
+) as NodeListOf<HTMLButtonElement>;
+
+const modal = document.querySelector(".modal") as HTMLDivElement;
+
+const overlay = document.querySelector(".overlay") as HTMLDivElement;
+
+openModalBtns.forEach((btn: HTMLButtonElement) => {
+	btn.addEventListener("click", () => {
+		modal.classList.remove("hidden");
+		overlay.classList.remove("hidden");
+	});
+});
+
+closeModalBtns.forEach((btn: HTMLButtonElement) => {
+	btn.addEventListener("click", () => {
+		modal.classList.add("hidden");
+		overlay.classList.add("hidden");
+	});
+});
