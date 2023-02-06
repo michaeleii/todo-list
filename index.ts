0e1a46251a72125a3f54f87f8f242f8ac1763756
Michael Lei <michaelleie0ca@gmail.com>
2023-02-05 16:43:17 -0800

    Add function to display to do list titles on webpage

--- index.ts
+++ index.ts
@@ -1,6 +1,16 @@
 import { ToDo } from "./todo";
 import { ToDoList } from "./todo-list";
 const addButton = document.getElementById("addTaskBtn") as HTMLButtonElement;
+const modal = document.querySelector(".modal") as HTMLDivElement;
+const overlay = document.querySelector(".overlay") as HTMLDivElement;
+const toDoList = new ToDoList();
+const openModalBtns = document.querySelector(
+	".show-modal"
+) as HTMLButtonElement;
+
+const closeModalBtns = document.querySelector(
+	".close-modal"
+) as HTMLButtonElement;
 
 const addToList = () => {
 	const title = document.getElementById("title") as HTMLInputElement;
@@ -9,39 +19,36 @@ const addToList = () => {
 	) as HTMLInputElement;
 	const dueDate = document.getElementById("dueDate") as HTMLInputElement;
 
-	const toDoList = new ToDoList();
 	const toDo = new ToDo(
 		title.value,
 		description.value,
 		dueDate.valueAsDate as Date
 	);
 	toDoList.addToDo(toDo);
-	console.log(toDoList.getToDoList());
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
+	updateToDoListContainer();
 };
 addButton.addEventListener("click", addToList);
 
-const openModalBtns = document.querySelectorAll(
-	".show-modal"
-) as NodeListOf<HTMLButtonElement>;
-
-const closeModalBtns = document.querySelectorAll(
-	".close-modal"
-) as NodeListOf<HTMLButtonElement>;
-
-const modal = document.querySelector(".modal") as HTMLDivElement;
-
-const overlay = document.querySelector(".overlay") as HTMLDivElement;
-
-openModalBtns.forEach((btn: HTMLButtonElement) => {
-	btn.addEventListener("click", () => {
-		modal.classList.remove("hidden");
-		overlay.classList.remove("hidden");
-	});
+openModalBtns.addEventListener("click", () => {
+	modal.classList.remove("hidden");
+	overlay.classList.remove("hidden");
 });
 
-closeModalBtns.forEach((btn: HTMLButtonElement) => {
-	btn.addEventListener("click", () => {
-		modal.classList.add("hidden");
-		overlay.classList.add("hidden");
-	});
+closeModalBtns.addEventListener("click", () => {
+	modal.classList.add("hidden");
+	overlay.classList.add("hidden");
 });
+
+const updateToDoListContainer = () => {
+	const toDoListContainer = document.querySelector(
+		".todo-items"
+	) as HTMLDivElement;
+	toDoListContainer.innerHTML = toDoList
+		.getToDoList()
+		.map((toDo) => {
+			return `<button class="todo">${toDo.title}</button>`;
+		})
+		.join("");
+};
